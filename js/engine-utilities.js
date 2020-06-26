d1bb55f1133804a45d261fa0dd175929583d890e
“Marc <marc_piantone@live.fr>
2020-06-26 14:42:58 -0400

    Answer Part 1 game actually end

--- js/engine-utilities.js
+++ js/engine-utilities.js
@@ -7,7 +7,7 @@
 // The purpose of this function is to determine in which slot to place our next enemy.
 // The possibilities are 0, 1, 2, 3 or 4.
 const nextEnemySpot = (enemies) => {
-  // enemySpots will refer to the number of spots available (can you calculate it?)
+  // enemySpots will refer to the number of spots available (can you calculate it? from the space available in the screen = 375px(gamewidth)/75px(enemy width))
   const enemySpots = GAME_WIDTH / ENEMY_WIDTH;
 
   // To find out where to place an enemy, we first need to find out which are the spots available.
@@ -26,7 +26,7 @@ const nextEnemySpot = (enemies) => {
   // We will randomly try different spots until we find out that is available
   let candidate = undefined;
   while (candidate === undefined || spotsTaken[candidate]) {
-    // candidate is assigned a random number between 0 and enemySpots (not including enemySpots). (what number is enemySpots?)
+    // candidate is assigned a random number between 0 and enemySpots (not including enemySpots). (what number is enemySpots? between 0 and <=5)
     candidate = Math.floor(Math.random() * enemySpots);
   }
 
@@ -40,10 +40,10 @@ const nextEnemySpot = (enemies) => {
 // The parameter represents the DOM node to which we will add the background
 const addBackground = (root) => {
   // We create a new img DOM node.
-  const bg = document.createElement('img');
+  const bg = document.createElement("img");
 
   // We set its src attribute and the height and width CSS attributes
-  bg.src = 'images/stars.png';
+  bg.src = "images/stars.png";
   bg.style.height = `${GAME_HEIGHT}px`;
   bg.style.width = `${GAME_WIDTH}px`;
 
@@ -53,14 +53,14 @@ const addBackground = (root) => {
   // We don't want the enemies to go beyond the lower edge of the image
   // so we place a white div to hide the enemies after they reach the bottom.
   // To see what it does, you can comment out all the remaining lines in the function to see the effect.
-  const whiteBox = document.createElement('div');
+  const whiteBox = document.createElement("div");
 
   // We put a high z-index so that the div is placed over all other DOM nodes
   whiteBox.style.zIndex = 100;
-  whiteBox.style.position = 'absolute';
+  whiteBox.style.position = "absolute";
   whiteBox.style.top = `${GAME_HEIGHT}px`;
   whiteBox.style.height = `${ENEMY_HEIGHT}px`;
   whiteBox.style.width = `${GAME_WIDTH}px`;
-  whiteBox.style.background = '#fff';
+  whiteBox.style.background = "#fff";
   root.append(whiteBox);
 };
